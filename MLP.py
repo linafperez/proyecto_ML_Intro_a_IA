f005e6c0ad20ebb1201cbbe23a38f207b6e512e0
Phel <juanf3l1405@gmail.com>
2026-05-25 14:06:25 -0500

    Implementacion inicial de MLP

---
+++ MLP.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Input
+from keras.optimizers import Optimizer
+
+# Ruta de los datos
+DATA_PATH = "data/processed/ml_ready/"
+
+# Importar los datos de entrenamiento y validacion
+x_train = np.load(os.path.join(DATA_PATH, "X_training.npy"))
+y_train = np.load(os.path.join(DATA_PATH, "y_training.npy"))
+x_val = np.load(os.path.join(DATA_PATH, "X_validation.npy"))
+y_val = np.load(os.path.join(DATA_PATH, "y_validation.npy"))
+
+print("Caracteristicas de entrenamiento (x_train):", x_train.shape)
+print("Etiquetas de entrenamiento (y_train):", y_train.shape)
+print("Caracteristicas de validacion (x_test):", x_val.shape)
+print("Etiquetas de validacion (y_test):", y_val.shape)
+print("Cantidad de imagenes de prueba: ", x_train.shape[0])
+
+num_features = x_train.shape[1]
+
+model = Sequential([
+    Input(shape=(num_features,)),
+    Dense(256, activation='sigmoid', input_shape=(num_features,)),
+    Dense(128, activation='sigmoid'),
+    Dense(2, activation='softmax'),
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+mod = model.fit(x_train, y_train, 
+                epochs=30,
+                batch_size=64,
+                validation_data=(x_val, y_val))
+
+results = model.evaluate(x_val, y_val, verbose=0)
+print('Test loss, Test accuracy: ', results)
